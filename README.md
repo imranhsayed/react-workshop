0c988264986a90f3e1d3f2b7f7d405d559ad34ab
Imran Sayed <imranhsayed@gmail.com>
2019-04-09 00:19:43 +0530

    Add Examples for JSX

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ I've branched the repo for individual topics.
 To checkout all branches run: `git branch --all`.
 To checkout individual branch run: `git checkout <branch name>`. From there on you should be good.
 
-## Branch Name: 01-react-app-webpack
+## Branch Name: 02-jsx-examples
 ## Branch Description:
-Set up React Application with using Webpack and Babel
+Examples of using JSX
 
 ## Installation
 
--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
 class App extends React.Component {
+
+	formatName( user ) {
+		return `Super ${user}`;
+	}
+
 	render() {
+
+		// Example One: Assign result to a const
+		const element = <h1>Hello World!</h1>;
+
+		// Example Two: Use {} for variables
+		const name = 'Josh Perez'
+		const elementTwo = <h1>Hello, {name}</h1>;
+
+		// Example Three: Call functions inside {}
+		const elementThree = (
+			<h1>
+				Hello, { this.formatName( 'rtUser' ) }
+			</h1>
+		);
+
+		// Example Four: Use of Object properties
+		const user = {
+			avatarUrl: 'https://via.placeholder.com/150'
+		};
+		const elementFour = <img src={ user.avatarUrl } />;
+
 		return (
-			<div>My App Component</div>
+			<div>
+				My App Component
+				{ element }
+				{ elementTwo }
+				{ elementThree }
+				{ elementFour }
+			</div>
 		);
 	}
 }
