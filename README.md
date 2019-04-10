e0cd1fd522f8292a1eb84578c514d753cc0d91c8
Imran Sayed <imranhsayed@gmail.com>
2019-04-10 11:40:33 +0530

    Add Input refs

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ I've branched the repo for individual topics.
 To checkout all branches run: `git branch --all`.
 To checkout individual branch run: `git checkout <branch name>`. From there on you should be good.
 
-## Branch Name: 01-react-app-webpack
+## Branch Name: 03-refs
 ## Branch Description:
-Set up React Application with using Webpack and Babel
+Example for using React refs
+React.createRef() creates a reference that can be attached to React elements via the ref attribute.
 
 ## Installation
 
--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class App extends React.Component {
+
+	constructor( props ) {
+		super( props );
+		this.inputRef = React.createRef();
+		this.inputRefTwo = React.createRef()
+		console.log( 'refs not available during initial render', this.inputRef );
+	}
+
+	onChange = () => {
+		console.log( 'inputValue: ', this.inputRef.current.value );
+		console.log( 'inputValue2: ', this.inputRefTwo.current.value );
+	}
+
+	componentDidMount() {
+		console.log( 'inputElement: ', this.inputRef );
+	}
+
 	render() {
 		return (
-			<div>My App Component</div>
+			<div>
+				<input type="text" ref={ this.inputRef } onChange={ this.onChange }/>
+				<input type="text" ref={ this.inputRefTwo } onChange={ this.onChange }/>
+			</div>
 		);
 	}
 }
