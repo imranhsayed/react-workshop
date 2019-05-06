b3ddbe61fcf19a0bc27988fe4ee3c097102a5a46
Imran Sayed <imranhsayed@gmail.com>
2019-05-07 00:00:03 +0530

    Add historyapi fallback config in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
 	output: {
 		path: path.resolve( __dirname, 'dist' ),
 		filename: 'main.js',
+		publicPath: '/',
+	},
+	devServer: {
+		historyApiFallback: true
 	},
 	module: {
 		rules: [
