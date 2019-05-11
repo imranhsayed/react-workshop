92489a33ec79af231b6d795abf3362bc2325f2a6
Imran Sayed <imranhsayed@gmail.com>
2019-05-11 21:04:56 +0530

    Update  HTML5 History API

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,32 @@ import { Link } from '@reach/router';
 export default () => {
 	return(
 		<div>
-			<Link to="/about">About</Link>
-			<Link to="/contact-us">Contact Us</Link>
+			<Link
+				style={{
+				margin: '16px',
+				padding: '16px 20px',
+					border: '1px solid black',
+					display: 'inline-block',
+					textDecoration: 'none',
+					color: '#555'
+				}}
+				to="/about"
+			>
+				About
+			</Link>
+			<Link
+				style={{
+					margin: '20px',
+					padding: '16px 20px',
+					border: '1px solid black',
+					display: 'inline-block',
+					textDecoration: 'none',
+					color: '#555'
+				}}
+				to="/contact-us"
+			>
+				Contact Us
+			</Link>
 		</div>
 	)
 }
