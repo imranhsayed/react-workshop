24741fc9fe6bdf4495c387ca5567f6fd47cc3155
Imran Sayed <imranhsayed@gmail.com>
2019-05-31 00:51:43 +0530

    Add app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import renderHTML from 'react-render-html';
 
 class App extends React.Component {
+
+	constructor( props ) {
+		super( props );
+		this.state = {
+			content: 'Hello world',
+			query: 'world',
+		};
+	}
+
+	wrapInTag = ( options ) => {
+		const tag = options.tag || 'strong',
+		    words = options.words || [],
+		    regex = RegExp(words.join('|'), 'gi'),
+		    replacement = '<'+ tag +'>$&</'+ tag +'>',
+		      content = this.state.content;
+
+		const modifiedContent = content.replace( regex, replacement );
+		this.setState( { content: modifiedContent } );
+	};
+
+	componentDidMount() {
+		this.wrapInTag({
+			tag: 'strong',
+			words: [ this.state.query ]
+		})
+	}
+
 	render() {
+
+		const { content } = this.state;
+
 		return (
-			<div>My App Component</div>
+			<div>
+				<p>{ renderHTML( content ) }</p>
+			</div>
 		);
 	}
 }
