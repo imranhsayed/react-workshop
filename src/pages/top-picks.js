f9d03babe123c4b8f77e61ac51290cb2399c9acf
Imran <imranhsayed@gmail.com>
2021-05-29 17:43:53 +0530

    Update files

--- src/pages/top-picks.js
+++ src/pages/top-picks.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import Layout from '../components/layout';
-
 const TopPicks = () => {
   return (
     <Layout>
-      Top picks
+      <h1 className="text-xl font-bold mb-4">Top picks</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam aut deserunt expedita, hic id, ipsum itaque iusto laborum maxime nam neque praesentium, quaerat quia repudiandae sint tempora tenetur totam!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam aut deserunt expedita, hic id, ipsum itaque iusto laborum maxime nam neque praesentium, quaerat quia repudiandae sint tempora tenetur totam!</p>
     </Layout>
   )
 }
-
-export default TopPicks
